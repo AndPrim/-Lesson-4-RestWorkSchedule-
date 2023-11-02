4eeb2cdbf6d3b846726a33bed8253d51bbea1475
AndPrim <andreyminoss@gmail.com>
2023-11-02 20:52:39 +0500

    TwoCommit

--- src/test/java/ru/netology/services/RestWorkSchedule/services/CountServiceTest.java
+++ src/test/java/ru/netology/services/RestWorkSchedule/services/CountServiceTest.java
@@ -2,24 +2,30 @@ package ru.netology.services.RestWorkSchedule.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class CountServiceTest {
-    @Test
-    void calculationOfTheNumberOfMonthsOfRest() {
+    @ParameterizedTest
+    @CsvSource({
+            "20000,3000,10000,3",
+            "150000,60000,100000,2"
+    })
+    void calculationOfTheNumberOfMonthsOfRest(int threshold, int expenses, int income, int realCount) {
         CalcOfMonthsOfRest service = new CalcOfMonthsOfRest();
 
-        int threshold = 20_000;
-        int expenses = 3_000;
-        int income = 10_000;
-        int realCount = 3;
+        //int threshold = 20_000;
+        //int expenses = 3_000;
+        //int income = 10_000;
+        //int realCount = 3;
 
         int actualCount = service.calcMonthRest(income, expenses, threshold);
 
         Assertions.assertEquals(realCount, actualCount);
     }
 
-    @Test
+    /*@Test
     void calculationOfTheNumberOfMonthsOfRestTwo() {
         CalcOfMonthsOfRest service = new CalcOfMonthsOfRest();
 
@@ -31,5 +37,5 @@ public class CountServiceTest {
         int actualCount = service.calcMonthRest(income, expenses, threshold);
 
         Assertions.assertEquals(realCount, actualCount);
-    }
+    }*/
 }
